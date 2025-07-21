47830e06761ef950ed16e8671316e043c0b03474
c3622379 <e.everett2655@student.leedsbeckett.ac.uk>
2025-07-21 01:34:42 +0000

    test

--- init.php
+++ init.php
@@ -8,7 +8,7 @@ if(session_status() == PHP_SESSION_NONE) {
 }
 
 $hostname = 'tangleserver.mysql.database.azure.com';
-$username = 'adminuser@tangleserver'; 
+$username = 'adminuser'; 
 $password = 'Password12345!';
 $databaseName = 'tangledatabase';
 
